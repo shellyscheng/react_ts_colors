bcc48eb9a0197d25fb54b2dc2c4e73381c589146
shellyscheng <shellysc13@gmail.com>
2023-04-15 19:49:08 -0700

    feat: use a custom createContext

--- src/context.tsx
+++ src/context.tsx
@@ -1,25 +1,24 @@
-import { Dispatch, PropsWithChildren, createContext, useReducer } from 'react';
+import { Dispatch, PropsWithChildren, useReducer } from 'react';
 import {
   AdjustColorActions,
   colorReducer,
   initialState,
 } from './lib/color-reducers';
+import { createContext } from './create-context';
 
 type ColorContextState = {
   hexColor: string;
   dispatch: Dispatch<AdjustColorActions>;
 };
 
-export const ColorContext = createContext<ColorContextState>({
-  hexColor: '#FFADEF',
-} as ColorContextState);
+const [useColorContext, ContextProvider] = createContext<ColorContextState>();
+
+export const useContext = useColorContext;
 
 export const ColorProvider = ({ children }: PropsWithChildren) => {
   const [{ hexColor }, dispatch] = useReducer(colorReducer, initialState);
 
   return (
-    <ColorContext.Provider value={{ hexColor, dispatch }}>
-      {children}
-    </ColorContext.Provider>
+    <ContextProvider value={{ hexColor, dispatch }}>{children}</ContextProvider>
   );
 };
